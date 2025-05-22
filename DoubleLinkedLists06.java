9d51e9ff179086f1bf59e6f1900944f1f0d9fc22
daffaputra09 <daffaputra863@gmail.com>
2025-05-22 13:55:35 +0700

    feat: modifikasi pertanyaan praktikum 2

--- DoubleLinkedLists06.java
+++ DoubleLinkedLists06.java
@@ -89,12 +89,16 @@ public class DoubleLinkedLists06 {
             System.out.println("List kosong, tidak bisa dihapus.");
             return;
         }
+
+        Mahasiswa06 tmp = head.data;
         if (head == tail) {
             head = tail = null;
         } else {
             head = head.next;
             head.prev = null;
         }
+        System.out.println("Data sudah berhasil dihapus. Data yang terhapus adalah:");
+        tmp.tampil();
     }
 
     public void removeLast() {
@@ -102,11 +106,15 @@ public class DoubleLinkedLists06 {
             System.out.println("List kosong, tidak bisa dihapus.");
             return;
         }
+        
+        Mahasiswa06 tmp = tail.data;
         if (head == tail) {
             head = tail = null;
         } else {
             tail = tail.prev;
             tail.next = null;
         }
+        System.out.println("Data sudah berhasil dihapus. Data yang terhapus adalah:");
+        tmp.tampil();
     }
 }
